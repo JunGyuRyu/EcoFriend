0e614e39ee6df4501cbfaca0dfce77143a089588
hush0428 <hush0428@naver.com>
2022-09-08 00:23:50 +0900

    first commit

---
+++ EcoGreen/app/src/main/kotlin/my/application/ecogreen/datas/PdfItem.kt
@@ -0,0 +1,12 @@
+package my.application.ecogreen.datas
+
+import java.io.Serializable
+
+class PdfItem(
+    var classification: String?,
+    var item: String?,
+    var standard: String?,
+    var levy_amt: Long?,
+    var count: Long?,
+) : Serializable {
+}
